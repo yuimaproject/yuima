4900bca9b1818c87d9c6d2c12fa89048c8346a62
iacus <iacus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-24 15:37:26 +0000

    added few explanations
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/yuima/pkg/yuima@23 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/simulate.R
+++ R/simulate.R
@@ -1,3 +1,12 @@
+## We have splitted the simulate function into blocks to allow for future 
+## methods to be added. S.M.I. & A.B.
+## Interface to simulate() changed to match the S3 generic funciton in the 
+## package stats
+## added an environment to let R find the proper values defined in the main
+## body of the function, which in turn calls different simulation methods
+## All new simulation methods should look into the yuimaEnv for local variables
+## when they need to "eval" R expressions
+
 ##:: function simulate
 ##:: solves SDE and returns result
 setGeneric("simulate",
