161eb11425f4ad20fdcbb7a827794bc8469e4531
iacus <iacus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-24 02:30:47 +0000

    added subsampling example
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/yuima/pkg/yuima@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/subsampling.R
+++ R/subsampling.R
@@ -1,13 +1,76 @@
-## We have splitted the simulate function into blocks to allow for future 
-## methods to be added. S.M.I. & A.B.
-## Interface to simulate() changed to match the S3 generic funciton in the 
-## package stats
-## added an environment to let R find the proper values defined in the main
-## body of the function, which in turn calls different simulation methods
-## All new simulation methods should look into the yuimaEnv for local variables
-## when they need to "eval" R expressions
 
-##:: function simulate
-##:: solves SDE and returns result
-subsampling <- function(x,y) return(x)
+##:: function subsampling
+##:: takes any yuima object with data or a yuima.data object and
+##:: performs subsampling according to some method
+
+# poisson.random.sampling
+# returns sample of data using poisson sampling
+
+setGeneric("subsampling", 
+function(x, sampling=NULL, Initial, Terminal, delta, 
+ grid = as.numeric(NULL), random = FALSE, sdelta=as.numeric(NULL), 
+ sgrid=as.numeric(NULL), interpolation="none") 
+ standardGeneric("subsampling")
+)
+
+setMethod("subsampling","yuima", 
+function(x, sampling=NULL, Initial, Terminal, delta, 
+ grid = as.numeric(NULL), random = FALSE, sdelta=as.numeric(NULL), 
+ sgrid=as.numeric(NULL), interpolation="none")
+ return(subsampling(x@data, sampling=sampling, Initial = Initial, 
+  Terminal = Terminal, delta = delta, 
+  grid = grid, random = random, sdelta=sdelta, 
+  sgrid=sgrid, interpolation=interpolation))
+)
+
+setMethod("subsampling", "yuima.data",
+function(x, sampling=sampling, Initial, Terminal, delta, 
+ grid = as.numeric(NULL), random = FALSE, sdelta=as.numeric(NULL), 
+ sgrid=as.numeric(NULL), interpolation="none"){
+
+ tmpsamp <- NULL
+ if(missing(sampling)){
+	tmpsamp <- setSampling(Initial = Initial, Terminal = Terminal, 
+				delta = delta, grid = grid, random = random, sdelta=sdelta, 
+				sgrid=sgrid, interpolation=interpolation)
+ } else {
+	tmpsamp <- sampling
+ }
+
+ 
+ Data <- get.zoo.data(x)
+ tmpgrid <- NULL
+
+# random sampling
+	 if(is.list(tmpsamp@random)){
+		 rdist <- c(tmpsamp@random$rdist)
+		 if(is.null(rdist))
+			stop("provide at least `rdist' argument for random sampling")
+		 n.rdist <- length(rdist)
+		 n.data <- length(Data)
+		 tmpgrid <- vector(n.data, mode="list")
+		 r.gen <- rep( rdist, n.data) # eventually reciclying arguments
+		 r.gen <- r.gen[1:n.data]
+		 for(i in 1:n.data){
+			 tmptime <- start(Data[[i]])	
+			 T <- end(Data[[i]])
+			 while(	sum( tmptime ) < T )
+				tmptime <- c(tmptime, r.gen[[i]](1))
+			 tmpgrid[[i]] <- cumsum(tmptime)
+			 if(tail(tmpgrid[[i]],1)>T)
+				tmpgrid[[i]] <- tmpgrid[[i]][-length(tmpgrid[[i]])]
+		 }
+	 }
+
+	 otime <- vector(n.data, mode="list")
+	 
+	 for(i in 1:n.data){
+	  otime[[i]] <- time(Data[[i]]) 
+	  idx <- as.numeric(sapply(tmpgrid[[i]], function(x) max(which(otime[[i]] <= x))))
+	  x@zoo.data[[i]] <- zoo(as.numeric(Data[[i]][idx]), order.by=tmpgrid[[i]])	 
+	 }
+	 return(x)
+ } ### end method
+)
+
 
