bc12671ef0a56b1b028db9d8dd2553d96847746b
iacus <iacus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-06-20 22:13:50 +0000

    fixed intro
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/yuima/pkg/yuima@79 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/zzz.R
+++ R/zzz.R
@@ -7,7 +7,7 @@
  require(methods)
  
  require(zoo)
- cat("\n\nThis is YUIMA Project package.\nNon stable release. Use at your own risk!!!\nFunctions and documentation may change in the final release\n\n")
+ cat(sprintf("\n\n%s\nThis is YUIMA Project package.\nNon stable release. Use at your own risk!!!\nFunctions and documentation may change in the final release\n%s\n\n",paste(rep("#",60),collapse=""),paste(rep("#",60),collapse="")))
 # require(KernSmooth, quietly=TRUE)
 # library.dynam("yuima", pkgname, libname) 
 }
