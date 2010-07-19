43c7f962e28825b34542497e520fe6dd5ec9eb8b
iacus <iacus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-07-19 10:14:24 +0000

    added lasso files as well
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/yuima/pkg/yuima@107 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/lasso.R
@@ -0,0 +1,61 @@
+# Initial version of lasso estimation for SDEs
+
+lasso <- function(yuima, lambda0, start, ...){
+	
+	call <- match.call()
+	
+	if( missing(yuima))
+		yuima.stop("yuima object 'yuima' is missing.")
+	
+	if( missing(lambda0) ){
+	 pars <- yuima@model@parameter@all	
+	 lambda0 <- rep(1, length(pars))
+	 names(lambda0) <- pars
+	 lambda0 <- as.list(lambda0)	
+	}
+	
+## FIXME: maybe we should choose initial values at random within lower/upper
+##        at present, qmle stops	
+	if( missing(start) ) 
+		yuima.stop("Starting values for the parameters are missing.")
+	
+	fail <- lapply(lambda0, function(x) as.numeric(NA))
+	
+	cat("\nLooking for MLE estimates...\n")
+	fit <- try(qmle(yuima, start=start,...), silent=TRUE)
+	if(class(fit)=="try-error")
+	return(list(mle=fail, sd.mle=NA, lasso=fail, sd.lasso=NA))
+	
+	SIGMA <- try( sqrt(diag(vcov(fit))), silent=TRUE)
+	if(class(SIGMA)=="try-error")
+	return(list(mle=fail, sd.mle=NA, lasso=fail, sd.lasso=NA))
+	
+	
+	theta.mle <- coef(fit)
+	
+	H <- try( solve(vcov(fit)), silent=TRUE) 
+	
+	if(class(H)=="try-error")
+	return(list(mle=fail, sd.mle=NA, lasso=fail, sd.lasso=NA))
+	
+	
+	lambda <- unlist(lambda0[names(theta.mle)])/abs(theta.mle)
+	
+	f2 <- function( theta ) as.numeric( t(theta-theta.mle) %*% H %*% (theta-theta.mle) + lambda %*% abs(theta) )
+	
+	cat("\nPerforming LASSO estimation...\n")
+	
+	fit2 <- try( optim(theta.mle, f2, hessian=TRUE,..., 
+					   control = list(maxit=30000, temp=2000, REPORT=500)), silent=TRUE) 
+	if(class(fit2)=="try-error")
+	return(list(mle=fail, sd.mle=NA, lasso=fail, sd.lasso=NA))
+	
+	theta.lasso <- fit2$par
+	
+	SIGMA1 <- try(sqrt(diag(solve(fit2$hessian))), silent=TRUE)
+	
+	if(class(SIGMA1)=="try-error")
+	return(list(mle=fail, sd.mle=NA, lasso=fail, sd.lasso=NA))
+	
+	return(list(mle=theta.mle, sd.mle=SIGMA, lasso=theta.lasso, sd.lasso=SIGMA1,call=call, lambda0=lambda0))
+}
