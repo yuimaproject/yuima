475f811a29c0dc5e6f5b46508076e2f879ad0d7a
iacus <iacus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-04-29 11:26:47 +0000

    added print method for lasso
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/yuima/pkg/yuima@309 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/lasso.R
+++ R/lasso.R
@@ -2,14 +2,21 @@
 # fixes a small bug in passing arguments to the
 # inner optim function
 
-lasso <- function (yuima, lambda0, start, delta = 1, ...) 
+lasso <- function (yuima, lambda0, start, delta = 1, ...)
 {
     call <- match.call()
     if (missing(yuima)) 
     yuima.stop("yuima object 'yuima' is missing.")
+    pars <- yuima@model@parameter@all
+    npars <- length(pars)
     if (missing(lambda0)) {
-        pars <- yuima@model@parameter@all
-        lambda0 <- rep(1, length(pars))
+        lambda0 <- rep(1, npars)
+        names(lambda0) <- pars
+        lambda0 <- as.list(lambda0)
+    }
+    if(!is.list(lambda0)){
+        lambda0 <- as.numeric(lambda0)
+        lambda0 <- as.numeric(matrix(lambda0, npars,1))
         names(lambda0) <- pars
         lambda0 <- as.list(lambda0)
     }
@@ -18,16 +25,26 @@ lasso <- function (yuima, lambda0, start, delta = 1, ...)
     fail <- lapply(lambda0, function(x) as.numeric(NA))
     cat("\nLooking for MLE estimates...\n")
     fit <- try(qmle(yuima, start = start, ...), silent = TRUE)
-    if (class(fit) == "try-error") 
-    return(list(mle = fail, sd.mle = NA, lasso = fail, sd.lasso = NA))
+    if (class(fit) == "try-error"){
+      tmp <- list(mle = fail, sd.mle = NA, lasso = fail, sd.lasso = NA)
+      class(tmp) <- "yuima.lasso"
+      return(tmp)
+    }
     theta.mle <- coef(fit)
     SIGMA <- try(sqrt(diag(vcov(fit))), silent = TRUE)
-    if (class(SIGMA) == "try-error") 
-    return(list(mle = theta.mle, sd.mle = NA, lasso = fail, sd.lasso = NA))
+    if (class(SIGMA) == "try-error"){
+     tmp <- list(mle = theta.mle, sd.mle = NA, lasso = fail, sd.lasso = NA)
+     class(tmp) <- "yuima.lasso"
+     return(tmp)
+    }
+
     H <- try(solve(vcov(fit)), silent = TRUE)
-    if (class(H) == "try-error") 
-    return(list(mle = theta.mle, sd.mle = SIGMA, lasso = fail, sd.lasso = NA))
-    
+    if (class(H) == "try-error"){
+      tmp <- list(mle = theta.mle, sd.mle = SIGMA, lasso = fail, sd.lasso = NA)
+      class(tmp) <- "yuima.lasso"
+      return(tmp)
+    }
+
     lambda <- unlist(lambda0[names(theta.mle)])/abs(theta.mle)^delta
     
     #lambda1 <- unlist(lambda0[names(theta.mle)])/abs(theta.mle)
@@ -48,79 +65,46 @@ lasso <- function (yuima, lambda0, start, delta = 1, ...)
     fit2 <- try( do.call(optim, args = args), silent = TRUE)
     
     
-    if (class(fit2) == "try-error") 
-    return(list(mle = theta.mle, sd.mle = SIGMA, lasso = fail, sd.lasso = NA))
+    if (class(fit2) == "try-error"){
+      tmp <- list(mle = theta.mle, sd.mle = SIGMA, lasso = fail, sd.lasso = NA)
+      class(tmp) <- "yuima.lasso"
+      return(tmp)
+    }
     theta.lasso <- fit2$par
     SIGMA1 <- try(sqrt(diag(solve(fit2$hessian))), silent = TRUE)
-    if (class(SIGMA1) == "try-error") 
-    return(list(mle = theta.mle, sd.mle = SIGMA, lasso = theta.lasso, 
-    sd.lasso = NA))
-    return(list(mle = theta.mle, sd.mle = SIGMA, lasso = theta.lasso, 
-    sd.lasso = SIGMA1, call = call, lambda0 = lambda0))
+    if (class(SIGMA1) == "try-error"){
+      tmp <- list(mle = theta.mle, sd.mle = SIGMA, lasso = theta.lasso,
+    sd.lasso = NA)
+      class(tmp) <- "yuima.lasso"
+      return(tmp)
+    }
+
+    tmp <- list(mle = theta.mle, sd.mle = SIGMA, lasso = theta.lasso,
+    sd.lasso = SIGMA1, call = call, lambda0 = lambda0)
+    class(tmp) <- "yuima.lasso"
+    return(tmp)
+
 }
 
-# removed version
-old.lasso <- function(yuima, lambda0, start, delta=1, ...){
-	
-	call <- match.call()
-	
-	if( missing(yuima))
-		yuima.stop("yuima object 'yuima' is missing.")
-	
-	if( missing(lambda0) ){
-	 pars <- yuima@model@parameter@all	
-	 lambda0 <- rep(1, length(pars))
-	 names(lambda0) <- pars
-	 lambda0 <- as.list(lambda0)	
-	}
-	
-## FIXME: maybe we should choose initial values at random within lower/upper
-##        at present, qmle stops	
-	if( missing(start) ) 
-		yuima.stop("Starting values for the parameters are missing.")
-	
-	fail <- lapply(lambda0, function(x) as.numeric(NA))
-	
-	cat("\nLooking for MLE estimates...\n")
-	fit <- try(qmle(yuima, start=start,...), silent=TRUE)
-	if(class(fit)=="try-error")
-	return(list(mle=fail, sd.mle=NA, lasso=fail, sd.lasso=NA))
-	
-	SIGMA <- try( sqrt(diag(vcov(fit))), silent=TRUE)
-	if(class(SIGMA)=="try-error")
-	return(list(mle=fail, sd.mle=NA, lasso=fail, sd.lasso=NA))
-	
-	
-	theta.mle <- coef(fit)
-	
-	H <- try( solve(vcov(fit)), silent=TRUE) 
-	
-	if(class(H)=="try-error")
-	return(list(mle=fail, sd.mle=NA, lasso=fail, sd.lasso=NA))
-	
-	
-#	lambda <- unlist(lambda0[names(theta.mle)])/abs(theta.mle)
-	lambda <- unlist(lambda0[names(theta.mle)])/abs(theta.mle)^delta
-    lambda1 <- unlist(lambda0[names(theta.mle)])/abs(theta.mle)
-	idx <- which(lambda>1e4)
-	lambda[idx] <- 1e4 # lambda1[idx]
-	
-	f2 <- function( theta ) as.numeric( t(theta-theta.mle) %*% H %*% (theta-theta.mle) + lambda %*% abs(theta) )
-	
-	cat("\nPerforming LASSO estimation...\n")
-	
-	fit2 <- try( optim(theta.mle, f2, hessian=TRUE,..., 
-					   control = list(maxit=30000, temp=2000, REPORT=500)), silent=TRUE) 
-	if(class(fit2)=="try-error")
-	return(list(mle=fail, sd.mle=NA, lasso=fail, sd.lasso=NA))
-	
-	theta.lasso <- fit2$par
-	
-	SIGMA1 <- try(sqrt(diag(solve(fit2$hessian))), silent=TRUE)
-	
-	if(class(SIGMA1)=="try-error")
-	return(list(mle = theta.mle, sd.mle = NA, lasso = theta.lasso, sd.lasso = NA))
-#	return(list(mle=fail, sd.mle=NA, lasso=fail, sd.lasso=NA))
-	
-	return(list(mle=theta.mle, sd.mle=SIGMA, lasso=theta.lasso, sd.lasso=SIGMA1,call=call, lambda0=lambda0))
+
+
+
+print.yuima.lasso <- function(x,...){
+    cat("Adaptive Lasso estimation\n")
+    cat("\nCall:\n")
+    print(x$cal)
+    if(!is.null(x$mle) & !is.null(x$sd.mle)){
+        qmle.tab <- rbind(x$mle, x$sd.mle)
+        rownames(qmle.tab) <- c("Estimate", "Std. Error")
+        cat("\nQMLE estimates\n")
+        print(t(qmle.tab))
+    }
+    
+    if(!is.null(x$lasso) & !is.null(x$sd.lasso)){
+        lasso.tab <- rbind(x$lasso, x$sd.lasso)
+        rownames(lasso.tab) <- c("Estimate", "Std. Error")
+        cat("\nLASSO estimates\n")
+        print(t(lasso.tab))
+    }
 }
+
