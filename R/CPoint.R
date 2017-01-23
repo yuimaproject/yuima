7e4cebd27f858d4a29e828abf53dc83f664a3ea7
iacus <iacus@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2017-01-23 05:17:46 +0000

    fixed qmleR,qmleL
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/yuima/pkg/yuima@575 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/CPoint.R
+++ R/CPoint.R
@@ -1,37 +1,35 @@
 qmleL <- function(yuima, t, ...){
-	
-
+    call <- match.call()
 	times <- time(yuima@data@zoo.data[[1]])
 	minT <- as.numeric(times[1])
 	maxT <- as.numeric(times[length(times)])
-	
 	if(missing(t) )
 	 t <- mean(c(minT,maxT))
-
 	if(t<minT || t>maxT)
 	  yuima.stop("time 't' out of bounds")
 	grid <- times[which(times<=t)]
-	qmle(subsampling(yuima, grid=grid), ...)
+    tmp <- subsampling(yuima, grid=grid)
+    mydots <- as.list(call)[-1]
+    mydots$t <- NULL
+    do.call(qmle, args=mydots)
 }
 
-qmleR <- function(yuima, t, ...){
-	
-	
-	times <- time(yuima@data@zoo.data[[1]])
-	minT <- as.numeric(times[1])
-	maxT <- as.numeric(times[length(times)])
-	
-	if(missing(t) )
-	t <- mean(c(minT,maxT))
-	
-	if(t<minT || t>maxT)
-	 yuima.stop("time 't' out of bounds")
-	grid <- times[which(times>=t)]
-	qmle(subsampling(yuima, grid=grid), ...)
+qmleR <- function (yuima, t, ...)
+{
+    call <- match.call()
+    times <- time(yuima@data@zoo.data[[1]])
+    minT <- as.numeric(times[1])
+    maxT <- as.numeric(times[length(times)])
+    if (missing(t))
+    t <- mean(c(minT, maxT))
+    if (t < minT || t > maxT)
+    yuima.stop("time 't' out of bounds")
+    grid <- times[which(times >= t)]
+    tmp <- subsampling(yuima, grid = grid)
+    mydots <- as.list(call)[-1]
+    mydots$t <- NULL
+    do.call(qmle, args=mydots)
 }
-
-
-
 
 
 CPointOld <- function(yuima, param1, param2, print=FALSE, plot=FALSE){
