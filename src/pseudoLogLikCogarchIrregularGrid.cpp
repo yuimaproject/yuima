4893855abd42a9ebb472ae9013cc8d0d688a47b2
lorenzo <lorenzo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2022-09-05 22:42:53 +0000

    Fixed pseudoLogLikCogarchIrregularGrid.cpp
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/yuima/pkg/yuima@815 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/pseudoLogLikCogarchIrregularGrid.cpp
+++ src/pseudoLogLikCogarchIrregularGrid.cpp
@@ -44,18 +44,19 @@ double Irregular_PseudoLoglik_COG(int lengthObs,  arma::mat B, arma::mat Btilde,
               arma::vec DeltaG2, arma::vec Deltat) {
   double res = 0;
   double VarDeltaG = 0;
-  double penal = 100000.123;
+  double penal = 1000000000;
   arma::mat I = arma::eye<arma::mat>(B.n_rows, B.n_rows);
 // for(i in c(1:(lengthObs))){
   for(int i=0;i<lengthObs;i++){
     arma::mat DeltatB1 = expmat(B * Deltat[i]);
     arma::mat DeltatB2 = expmat(Btilde * Deltat[i]);
     VarDeltaG = a0 * Deltat[i] * bq / (bq - a1) + arma::as_scalar(ta * DeltatB2 * InvBtilde * (I - DeltatB2) * (state - stateMean));
-    state <- (I + DeltaG2[i] / V * e * ta) * DeltatB1 * state+a0 * DeltaG2[i] / V * e;
+    state = (I + DeltaG2[i] / V * e * ta) * DeltatB1 * state+a0 * DeltaG2[i] / V * e;
+    V = arma::as_scalar(a0 + ta*state);
   if(VarDeltaG>0){
-    res =res - 0.5*(arma::as_scalar((DeltaG2[i] / VarDeltaG )) + log(VarDeltaG) + log(2 * 3.141593));//
+    res = res - 0.5*(DeltaG2[i] / VarDeltaG  + log(VarDeltaG) + log(2 * 3.141593));
   }else{
-    res = res+VarDeltaG-penal;
+    res = res-penal;
   }
 }
 return res;
