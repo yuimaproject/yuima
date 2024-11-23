fa2e267d5098cae5e054368436f49f785215f286
onion <kosukekito38@gmail.com>
2024-11-23 02:15:08 +0000

    feat: calculate constant before loop in filter_mean_explicit

--- src/kalmanBucyFilter.cpp
+++ src/kalmanBucyFilter.cpp
@@ -126,10 +126,11 @@ arma::mat calc_filter_mean_explicit(arma::mat un_dr_sl, arma::mat un_dr_in, arma
     arma::mat alpha = un_dr_sl - vcov * ob_dr_sl.t() * inv_Sigma * ob_dr_sl;
     arma::mat exp_alpha_h = arma::expmat(alpha * delta);
     arma::mat deltaY_coeff = exp_alpha_h * vcov * ob_dr_sl.t() * inv_Sigma;
+    arma::mat intercept = (exp_alpha_h * un_dr_in + deltaY_coeff * ob_dr_in) * delta;
 
     for(int i = 1; i < n_data; i++){
         arma::vec mean_prev = mean.col(i - 1);
-        arma::vec mean_next = exp_alpha_h * mean_prev + exp_alpha_h * un_dr_in * delta + deltaY_coeff * deltaY.col(i-1) + deltaY_coeff * ob_dr_in * delta;
+        arma::vec mean_next = exp_alpha_h * mean_prev + deltaY_coeff * deltaY.col(i-1) + intercept;
         mean.col(i) = mean_next;
     }
 
