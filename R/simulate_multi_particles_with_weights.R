cd3d8a28ff1ff76f800e47bfa7aa07559bd090ed
onion <kosukekito38@gmail.com>
2025-03-01 19:38:04 +0900

    fix: remove nsim from args, fix styles

--- R/simulate_multi_particles_with_weights.R
+++ R/simulate_multi_particles_with_weights.R
@@ -1,5 +1,5 @@
-simulate_multi_particles_with_weights <- function(yuima, nsim, seed, xinits, true_parameter,
-    method, sampling) {
+simulate_multi_particles_with_weights <- function(yuima, xinits, true_parameter,
+    method, sampling, seed) {
     ## :: errors checks
 
     ## :1: error on yuima model
@@ -7,15 +7,17 @@ simulate_multi_particles_with_weights <- function(yuima, nsim, seed, xinits, tru
         yuima.warn("yuima object is missing.")
         return(NULL)
     }
-    if (!inherits(yuima@model, "yuima.state_space_model")) {
+
+    model <- yuima@model
+    if (!inherits(model, "yuima.state_space_model")) {
         yuima.warn("yuima object must be a stateSpaceModel.")
         return(NULL)
     }
-    if (yuima@model@hurst != 0.5) {
+    if (model@hurst != 0.5) {
         yuima.warn("Hurst parameter must be 0.5 for multi-particle simulation.")
         return(NULL)
     }
-    if (length(yuima@model@jump.coeff) != 0) {
+    if (length(model@jump.coeff) != 0) {
         yuima.warn("Model with jump coefficients is not supported.")
         return(NULL)
     }
@@ -28,68 +30,61 @@ simulate_multi_particles_with_weights <- function(yuima, nsim, seed, xinits, tru
         }
     }
 
-    sdeModel <- yuima@model
-    Terminal <- sampling@Terminal[1]
-    Initial <- sampling@Initial[1]
-
-    n <- sampling@n[1]
-    r.size <- sdeModel@noise.number
-    d.size <- sdeModel@equation.number
 
     ## :2: error on xinit
     if (missing(xinits) || is.null(xinits)) {
         yuima.warn("Initial values are missing.")
         return(NULL)
     }
-    # TODO: check if xinits is a 2-dim array of numerics TODO: consider the
-    # type of xinits TODO: maybe nsim is not needed if xinits is not a matrix,
-    # error
+    # TODO: check if xinits is a 2-dim array of numerics
+    # TODO: consider the type of xinits
     if (!is.matrix(xinits)) {
         yuima.warn("Initial values must be a matrix.")
         return(NULL)
     }
-    if (missing(nsim) || is.null(nsim)) {
-        nsim <- nrow(xinits)
-    }
 
-    if (nrow(xinits) != nsim) {
-        yuima.warn("Length of xinits missmatches with nsim.")
-        return(NULL)
-    }
+    d.size <- model@equation.number
     if (ncol(xinits) != d.size) {
-        yuima.warn("Length of items in xinits missmatches with the dimension of the model.")
+        yuima.warn("ncol(xinits) missmatches with the dimension of the model.")
         return(NULL)
     }
 
-
-    par.len <- length(sdeModel@parameter@all)
-
-    if (missing(true_parameter) & par.len > 0) {
-        true_parameter <- vector(par.len, mode = "list")
-        for (i in 1:par.len) {
+    all_parameters <- model@parameter@all
+    npar <- length(all_parameters)
+    if (missing(true_parameter) & npar > 0) {
+        true_parameter <- vector(npar, mode = "list")
+        for (i in 1:npar) {
             true_parameter[[i]] <- 0
         }
-        names(true_parameter) <- sdeModel@parameter@all
+        names(true_parameter) <- all_parameters
     }
 
     yuimaEnv <- new.env()
 
-    if (par.len > 0) {
-        for (i in 1:par.len) {
-            pars <- sdeModel@parameter@all[i]
+    if (npar > 0) {
+        for (i in 1:npar) {
+            par_name <- all_parameters[i]
 
             for (j in 1:length(true_parameter)) {
-                if (is.na(match(pars, names(true_parameter)[j])) != TRUE) {
-                  assign(sdeModel@parameter@all[i], true_parameter[[j]], yuimaEnv)
+                if (par_name == names(true_parameter)[j]) {
+                  assign(par_name, true_parameter[[j]], yuimaEnv)
                 }
             }
         }
     }
 
-    ## :: using Euler-Maruyama method
-    delta <- (Terminal - Initial)/n
+    # prepare values of random noise
+    delta <- sampling@delta
+    nsim <- nrow(xinits)
+    n <- sampling@n[1]
+    r.size <- model@noise.number
+
+    if (!missing(seed)) {
+        set.seed(seed)
+    }
     dW <- rnorm(nsim * n * r.size, 0, sqrt(delta))
 
+    ## simulate using Euler-Maruyama method
     data <- euler_multi_particles_with_weights(xinits, yuima, dW, yuimaEnv)
 
     return(data)
